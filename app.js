f2416bcfe9a1aeeb9351635fe05cdad8ee84f6e0
Kelly Dias <84358631+keelly-18@users.noreply.github.com>
2023-12-09 16:13:58 -0300

    Add files

---
+++ app.js
@@ -0,0 +1,17 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let outraListaGenerica = ['Cachorro', 'Gato', 'Macaco'];
+console.log(outraListaGenerica[0])
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let mesaLista = ['Celular', 'Computador', 'Mouse'];
+console.log(mesaLista[1]);
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaMesa = ['Caneta', 'Lapis', 'Borracha'];
+console.log(listaMesa[2]);
